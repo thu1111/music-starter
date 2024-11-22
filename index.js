7a668247d276bb85c6ac0a7779173d29060276a4
Thu Yein Tun <yon.fu111@gmail.com>
2024-11-22 15:30:43 -0500

    initial commit

--- index.js
+++ index.js
@@ -1,27 +1,45 @@
 const COHORT = "REPLACE_ME!";
 const API_URL = `https://fsa-crud-2aa9294fe819.herokuapp.com/api/${COHORT}/artists`;
 
-// === State ===
+//1.State 
 
 const state = {
   artists: [],
 };
 
-/** Updates state with artists from API */
+//2. Get Data -  Updates state with artists from API
 async function getArtists() {
   // TODO
+  try {
+    const response = await fetch (API_URL);
+    const json = await response.json();
+    state.artists = json.data;
+  } catch (error) {
+    console.log(error);    
+  }
 }
 
-/** Asks the API to create a new artist based on the given `artist` */
-async function addArtist(artist) {
-  // TODO
-}
-
-// === Render ===
-
-/** Renders artists from state */
+//3. Render
 function renderArtists() {
   // TODO
+  const artistList = document.querySelector("#artists"); //DOM link with ul
+
+  if (!state.artists.length) {
+    recipesList.innerHTML = "<li>No artists.</li>";
+    return;
+  }
+
+  const artistCards = state.artists.map((artist)=>{
+    const card = document.createElement("li");
+    card.innerHTML=`
+      <h2>${artist.name}</h2>
+      <img src="${artist.imageUrl}" alt="${artist.name}"/>
+      <p></p>
+    `;
+    return card;
+  });
+
+  artistList.replaceChildren(...artistCards);
 }
 
 /** Syncs state with the API and rerender */
@@ -29,9 +47,42 @@ async function render() {
   await getArtists();
   renderArtists();
 }
-
-// === Script ===
-
 render();
 
+//4. Add and Create
+/** Asks the API to create a new artist based on the given `artist` */
+async function addArtist(artist) {
+  // TODO
+  try {
+    const response = await fetch(API_URL,{
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(artist),
+    });
+    const json = await response.json();
+
+    if (json.error) {
+      throw new Error(json.error.message);
+    }
+
+    render();
+  } catch (error) {
+    console.log(error);    
+  }
+}
+
 // TODO: Add artist with form data when the form is submitted
+const form = document.querySelector("form");
+form.addEventListener("submit", async(event)=>{
+  event.preventDefault();
+
+  const artist = {
+    name: form.artistName.value,
+    description: form.description.value,
+    imageUrl: form.imageUrl.value,
+  }
+
+  await addArtist(artist);
+  // render();
+});
+
